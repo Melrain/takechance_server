0a4314e8c485561fff85f3ab8edd9c15909cf92d
Melrain <yezyd92@icloud.com>
2024-03-30 15:57:57 +0800

    2

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Express on Vercel'));
-
-app.post('/api', (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res.status(400).json({ message: 'Name is required' });
+app.get('/api', (req, res) => {
+  if (req.query.name) {
+    res.send(`Hello, ${req.query.name}`);
+  } else {
+    res.send('Hello, stranger');
   }
-  res.json({ message: `Hello, ${name}` });
 });
 
 app.listen(8000, () => console.log('Server ready on port 8000.'));
