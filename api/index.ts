ecf8fce3905ca2b104a2355713ef62dfd55635f8
Melrain <yezyd92@icloud.com>
2024-03-30 15:55:19 +0800

    add name express

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,14 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Express on Vercel'));
 
+app.post('/api', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: 'Name is required' });
+  }
+  res.json({ message: `Hello, ${name}` });
+});
+
 app.listen(8000, () => console.log('Server ready on port 8000.'));
 
 module.exports = app;
